ade7632d5340aa16771dd9119dc4c62cb11e85ba
Georgi Martinov <stunstyle@yahoo.com>
2018-09-14 19:58:57 +0300

    change PickDateView to use the custom DatePicker

--- src/main/java/com/stunstyle/miomart2/view/PickDateView.java
+++ src/main/java/com/stunstyle/miomart2/view/PickDateView.java
@@ -1,6 +1,7 @@
 package com.stunstyle.miomart2.view;
 
 import com.stunstyle.miomart2.presenter.PickDatePresenter;
+import com.stunstyle.miomart2.util.SimpleDatePicker;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.DateCell;
@@ -33,34 +34,9 @@ public class PickDateView extends GridPane {
         this.setVgap(10);
         this.setPadding(new Insets(25, 25, 25, 25));
 
-        datePicker = new DatePicker(LocalDate.now());
+        datePicker = new SimpleDatePicker(LocalDate.now());
 
-        Locale.setDefault(new Locale.Builder().setLanguage("bg").setRegion("BG").setScript("Cyrl").build());
-        // TODO: change first day of week
-        datePicker.setEditable(false);
-        datePicker.setConverter(new LocalDateStringConverter(FormatStyle.MEDIUM));
-        Callback<DatePicker, DateCell> dayCellFactory =
-                new Callback<DatePicker, DateCell>() {
-                    @Override
-                    public DateCell call(final DatePicker datePicker) {
-                        return new DateCell() {
-                            @Override
-                            public void updateItem(LocalDate item, boolean empty) {
-                                super.updateItem(item, empty);
 
-                                if (item.isAfter(LocalDate.now())) {
-                                    this.setDisable(true);
-                                }
-
-                                DayOfWeek day = DayOfWeek.from(item);
-                                if (day == DayOfWeek.SUNDAY) {
-                                    this.setTextFill(Color.RED);
-                                }
-                            }
-                        };
-                    }
-                };
-        datePicker.setDayCellFactory(dayCellFactory);
         Label chooseRecordLabel = new Label("Избор на запис");
         Button chooseRecordButton = new Button("Преглед");
         chooseRecordButton.setOnAction(actionEvent -> presenter.showEditRecord());
