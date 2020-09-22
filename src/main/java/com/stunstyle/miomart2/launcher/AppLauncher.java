8951e9db96e1ae96d5ae0588285e4b159b939484
Georgi Petrov <stunstyle@yahoo.com>
2020-09-23 01:34:48 +0300

    Update the shade plugin and allow launching the uber jar with java 11.

---
+++ src/main/java/com/stunstyle/miomart2/launcher/AppLauncher.java
@@ -0,0 +1,9 @@
+package com.stunstyle.miomart2.launcher;
+
+import com.stunstyle.miomart2.view.App;
+
+public class AppLauncher {
+	public static void main(String[] args) {
+		App.main(args);
+	}
+}
